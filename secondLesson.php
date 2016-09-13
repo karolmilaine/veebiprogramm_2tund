69e2520784cfce6c867ce794515a6b3ca229ddd7
karojyrg <karojyrg@greeny.cs.tlu.ee>
2016-09-13 13:10:37 +0300

    for loop & if statement

---
+++ secondLesson.php
@@ -0,0 +1,40 @@
+<?php
+
+$age = 19;
+
+//suurem võrdne ja väiksem võrdne kirjutatakse nii: >= <=
+//kas võrduvad kirjutatakse nii: ==
+//ei võrdu kirjutatakse nii: !=
+
+
+if ($age < 18){
+	echo "alaealine";
+} else {
+	echo "täisealine";
+}
+
+
+
+?>
+
+<br>
+
+<?php
+
+//for(üks kord kõige alguses nt $i=0;enne igat 
+//korda nt kontrollib kas $i<$age, pärast igat korda nt teeb i väärtuse alati suuremaks $i=$i+1)
+//eesmärk vanusele vastavalt
+//trükime välja 'palju'
+// kui trükime $i. enne "palju", siis loendab
+// kui muudan $i=1 siia alustab loendamist 1st, et tingimus püsiks ehk 19 siis kirjutas $i <=age
+	
+for ($i = 0; $i < $age; $i = $i + 1 ){
+	
+	echo $i."palju ";
+	
+	
+	
+}
+echo "õnne!";
+
+?>
